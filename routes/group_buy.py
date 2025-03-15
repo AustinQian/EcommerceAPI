c0428c3defe6c3450a0931c2b882f9d597bc888e
AustinQian <ubunteen2@gmail.com>
2025-03-14 20:07:07 -0400

    error fix for retreving participated users

--- routes/group_buy.py
+++ routes/group_buy.py
@@ -44,7 +44,6 @@ def create_group_buy():
 def get_group_buy(unique_link):
     # Retrieve details about a specific group buy using the unique link.
     group_buy = GroupBuy.query.filter_by(unique_link=unique_link).first()
-    participant_count = group_buy.get_participant_count()
     if not group_buy:
         return jsonify({'error': 'Group buy not found'}), 404
 
@@ -56,7 +55,7 @@ def get_group_buy(unique_link):
         'current_participants': group_buy.current_participants,
         'unique_link': group_buy.unique_link,
         'is_active': group_buy.is_active,
-        'participant_count': participant_count
+        'participant_count': group_buy.get_participant_count()
     }), 200
 
 @group_buy_bp.route('/join/<unique_link>', methods=['POST'])
